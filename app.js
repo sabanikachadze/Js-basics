e8fb316676ef55dae711fbc997f116a5dad2a9b7
sabanikachadze <nikachadzesaba027@gmail.com>
2023-11-25 16:30:18 +0400

    Js basic problem solutions

---
+++ app.js
@@ -0,0 +1,12 @@
+// const string = "Bla Bla";
+// function countCharacters(Text){
+// const historgram = {};
+// for(let i in Text){
+//   const ch = Text[i]
+//  historgram[ch] = (historgram[ch] || 0) + 1
+// };
+// return historgram
+// };
+// console.log(countCharacters(string))
+
+
